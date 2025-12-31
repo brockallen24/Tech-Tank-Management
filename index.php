02da7b8e23a62344f75a057461446c1f4344be5c
brockallen24 <ballen@tech-tank.com>
2025-12-30 19:54:46 -0500

    Add routing to inventory page in index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include_once("index.html"); ?>
+<?php 
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (strpos($request, 'inventory_custom_layout') !== false) {
+      include 'inventory_custom_layout.html';
+} else {
+      include 'index.html';
+}
+?>
